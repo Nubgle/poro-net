cea2e0eea2f6532dcb5b9affa9503533ec630a2e
Nubgle <32046884+Nubgle@users.noreply.github.com>
2018-06-10 16:49:39 -0600

    Update poronet.js

--- poronet.js
+++ poronet.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const UrbanDictionary = require("urban-dictionary");
+const pokegif = require("pokemon-gif");
+let Pokedex = require("pokedex")
 
 client.on("ready", () => {
   console.log("im about to oof up this city");
